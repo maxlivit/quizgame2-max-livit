0ca54e685bb3c4130a3ace9fe713775647673c0d
maxlivit <71710186+maxlivit@users.noreply.github.com>
2021-04-26 21:47:29 -0400

    finished

--- src/js/end.js
+++ src/js/end.js
@@ -3,32 +3,32 @@ const saveScoreBtn = document.querySelector("#saveScoreBtn");
 const finalScore = document.querySelector("#finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [] 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-const MAX_HIGH_SCORES = 5
+const MAX_HIGH_SCORES = 5;
 
-finalScore.innterText = mostRecentScore
+finalScore.innterText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value 
-}) //cant save score if there is no value for username 
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+}); //cant save score if there is no value for username
 
-saveHighScore = e => {
-    e.preventDefault ()
+saveHighScore = (e) => {
+  e.preventDefault();
 
-    const score = {
-        score: mostRecentScore, 
-        name = username.value
-    }
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
 
-    highScores.push(score)
+  highScores.push(score);
 
-    highScores.sort((a,b) => {
-        return b.score - a.score
-    })
+  highScores.sort((a, b) => {
+    return b.score - a.score;
+  });
 
-    highScores.splice(5)
+  highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
-}
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
+};
